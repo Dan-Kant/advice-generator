c961fd302b0ad6e16b5cb7f8de492640e0960d63
Dan Kant <87670742+Dan-Kant@users.noreply.github.com>
2022-11-14 14:39:21 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const adviceNumber = document.querySelector('.advice-number');
+const quote = document.querySelector('.quote');
+const randomBtn = document.querySelector('.random');
+
+
+function getQuote() {
+    fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+}).then(data => {
+    const dataObj = data.slip;
+    quote.innerHTML = `${dataObj.advice}`;
+    adviceNumber.innerHTML = `${dataObj.id}`;
+}).catch(error => {
+    console.log(error);
+});
+};
+
+randomBtn.addEventListener('click', getQuote);
